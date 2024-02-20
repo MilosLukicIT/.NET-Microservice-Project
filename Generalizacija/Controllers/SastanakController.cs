7600109f3d7b0f306305101b3e3e18893c5e0d13
MilosLukicIT <milos.lukich@gmail.com>
2024-02-20 15:09:39 +0100

    asdasd

---
+++ Generalizacija/Controllers/SastanakController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Generalizacija.Data;
+using Generalizacija.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Generalizacija.Controllers
+{
+
+    [ApiController]
+    [Route("api/sastanak")]
+    [Produces("application/json", "application/xml")]
+    public class SastanakController : Controller
+    {
+        private readonly ISastanakRepository sastanakRepository;
+        private readonly LinkGenerator linkGenerator;
+        private readonly IMapper mapper;
+
+
+        public SastanakController(ISastanakRepository sastanakRepository, LinkGenerator linkGenerator, IMapper mapper)
+        {
+            this.sastanakRepository = sastanakRepository;
+            this.linkGenerator = linkGenerator;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        public ActionResult<List<SastanakInfoDto>> Get()
+        {
+            var sastanci = sastanakRepository.GetAll();
+
+            if (sastanci == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(mapper.Map<List<SastanakInfoDto>>( sastanci));
+        }
+    }
+}
---
+++ Generalizacija/Dto/SastanakDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Generalizacija.Dto
+{
+    public class SastanakDto
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string nesto { get; set; }
+    }
+}
